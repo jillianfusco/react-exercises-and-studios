9a31fea4315e00342dfd498209b265a1ad3ec2fc
Jillian Fusco <fuscojillian@gmail.com>
2024-08-21 15:11:02 -0400

    edited movies and books

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I'm Reading";
+   let book1 = "https://a.media-amazon.com/images/I/51IAdEMjV5L._SY445_SX342_.jpg";
+   let book2 = "https://a.media-amazon.com/images/I/51OazXen1ZL._SY445_SX342_.jpg";
+   let book3 = "https://a.media-amazon.com/images/I/41VzZU97k9L._SY445_SX342_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Cassandra in Reverse" />
+         <img src={book2} alt="Legends and Lattes" />
+         <img src={book3} alt="Her Majesty's Royal Coven" />
       </div>      
    );
 }
